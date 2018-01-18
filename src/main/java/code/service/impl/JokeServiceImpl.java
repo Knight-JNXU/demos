16c5ee23212493caccdc5f967b7afb539c97f7ae
wen.liu <wen.liu@baozun.com>
2018-01-18 11:29:46 +0800

    引入缺失的 jackson-core package

--- src/main/java/code/service/impl/JokeServiceImpl.java
+++ src/main/java/code/service/impl/JokeServiceImpl.java
@@ -1,10 +1,17 @@
 package code.service.impl;
 
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.feilong.net.httpclient4.HttpClientUtil;
+
+import code.model.RequestParams;
 import code.service.JokeService;
 
 /**
@@ -20,8 +27,9 @@ public class JokeServiceImpl extends BaseServiceImpl implements JokeService{
     /** The Constant log. */
     private static final Logger LOGGER = LoggerFactory.getLogger(JokeServiceImpl.class);
     
-//    @Qualifier(value="jokeRequestParams")
-//    private RequestParams httpRequest;
+    @Autowired
+    @Qualifier(value="jokeRequestParams")
+    private RequestParams httpRequest;
     
     /**
      * 根据页码获取笑话
@@ -30,14 +38,16 @@ public class JokeServiceImpl extends BaseServiceImpl implements JokeService{
      * @return
      */
     public String getJokesByPage(String page) {
-        /*String requestUrl = httpRequest.buildeCompleteUrl(page, ""+(new Date().getTime()/1000));
-        String responseStr = HttpClientUtil.get(requestUrl);
-        LOGGER.info("聚合数据：笑话大全：request{}：response{}", requestUrl, responseStr);
-        return responseStr;*/
         
-        
-        
-        return "";
+        try{
+            String requestUrl = httpRequest.buildeCompleteUrl(page, ""+(new Date().getTime()/1000));
+            String responseStr = HttpClientUtil.get(requestUrl);
+            LOGGER.info("聚合数据：笑话大全：request{}：response{}", requestUrl, responseStr);
+            return responseStr;
+        }catch (Exception e){
+            LOGGER.error("获取笑话失败:{}", e.getStackTrace());
+            return "";
+        }
     }
     
 }
